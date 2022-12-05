bb83771d4bcde5a3a1b18b0311b5f55ece7236a4
David Nicholson <nickledave@users.noreply.github.com>
2022-12-04 22:22:27 -0500

    publish-to-github-force from

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,49 @@
+var store = [{
+        "title": "Comparison of machine learning methods applied to birdsong element classification",
+        "excerpt":"Dataset used for results in Scipy Proceedings paper, “Comparison of machine learning methods applied to birdsong element classification”. Proc. of the 15th Python in Science Conf (SciPy 2016)  Code for paper is in https://github.com/NickleDave/ML-comparison-birdsong      results_dict.json   same_ftrs_results_dict.json   knn_ftrs_results_dict.json   Google Drive link  ","categories": [],
+        "tags": [],
+        "url": "/datasets/2016-07-30-scipy-proceedings",
+        "teaser": null
+      },{
+        "title": "Supplementary Information for the dissertation \"Cerebellothalamic and thalamostriatal and pathways in a songbird, the Bengalese finch\"",
+        "excerpt":"Imaging data from neuroanatomical studies:  https://figshare.com/articles/Supplementary_Information_for_Thalamostriatal_and_cerebellothalamic_pathways_in_a_songbird_/5437975   Published in J. Comp. Neuro: http://onlinelibrary.wiley.com/doi/10.1002/cne.24428/abstract  First posted on biorxiv: https://www.biorxiv.org/content/10.1101/197590v1  Complete dissertation: https://etd.library.emory.edu/concern/etds/8k71nh08w  ","categories": [],
+        "tags": [],
+        "url": "/datasets/2017-11-07-cerebellothalamic-thalamostriatal-songbird",
+        "teaser": null
+      },{
+        "title": "Bengalese finch song repository",
+        "excerpt":"A collection of song from four Bengalese finches recorded in the Sober lab at Emory University. Link:  https://figshare.com/articles/Bengalese_Finch_song_repository/4805749   Used with hybrid-vocal-classifier library to benchmark machine learning algorithms for labeling birdsong syllables  and later with the vak  library to benchmark the tweetynet neural network architecture, in this paper.  ","categories": [],
+        "tags": [],
+        "url": "/datasets/2017-10-18-bengalese-finch-song-repository",
+        "teaser": null
+      },{
+        "title": "Make your own `academicpages` from scratch with `jekyll`",
+        "excerpt":"In this post I document the process I went through to set up a peronsal web site with a structure similar to the academicpages theme used by many academics. I’m writing the post for my future self, and for you if you want to do the same. Why would you...","categories": [],
+        "tags": [],
+        "url": "/how-to-use-jekyll-to-set-up-your-own-academic-website/",
+        "teaser": null
+      },{
+        "title": "Paper Title Number 1",
+        "excerpt":"This paper is about the number 1. The number 2 is left for future work.   Download paper here   Recommended citation: Your Name, You. (2009). “Paper Title Number 1.” Journal 1. 1(1).  ","categories": [],
+        "tags": [],
+        "url": "/publication/2009-10-01-paper-title-number-1",
+        "teaser": null
+      },{
+        "title": "Talk 1 on Relevant Topic in Your Field",
+        "excerpt":"This is a description of your talk, which is a markdown files that can be all markdown-ified like any other post. Yay markdown!  ","categories": [],
+        "tags": [],
+        "url": "/talks/2012-03-01-talk-1",
+        "teaser": null
+      },{
+        "title": "Tutorial 1 on Relevant Topic in Your Field",
+        "excerpt":"More information here   This is a description of your tutorial, note the different field in type. This is a markdown files that can be all markdown-ified like any other post. Yay markdown!  ","categories": [],
+        "tags": [],
+        "url": "/talks/2013-03-01-tutorial-1",
+        "teaser": null
+      },{
+        "title": "Teaching experience 1",
+        "excerpt":"This is a description of a teaching experience. You can use markdown like any other post.   Heading 1   Heading 2   Heading 3  ","categories": [],
+        "tags": [],
+        "url": "/teaching/2014-spring-teaching-1",
+        "teaser": null
+      }]

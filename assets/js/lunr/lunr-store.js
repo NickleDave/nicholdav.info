a19e8dac48f1612d1038a0ec7985f83158a32198
David Nicholson <NickleDave@users.noreply.github.com>
2024-07-12 14:06:00 -0400

    publish-to-github-force from

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,91 @@
+var store = [{
+        "title": "Comparison of machine learning methods applied to birdsong element classification",
+        "excerpt":"Dataset used for results in Scipy Proceedings paper, “Comparison of machine learning methods applied to birdsong element classification”. Proc. of the 15th Python in Science Conf (SciPy 2016)  Code for paper is in https://github.com/NickleDave/ML-comparison-birdsong      results_dict.json   same_ftrs_results_dict.json   knn_ftrs_results_dict.json   Google Drive link  ","categories": [],
+        "tags": [],
+        "url": "/datasets/2016-07-30-scipy-proceedings",
+        "teaser": null
+      },{
+        "title": "Supplementary Information for the dissertation \"Cerebellothalamic and thalamostriatal and pathways in a songbird, the Bengalese finch\"",
+        "excerpt":"Imaging data from neuroanatomical studies:  https://figshare.com/articles/Supplementary_Information_for_Thalamostriatal_and_cerebellothalamic_pathways_in_a_songbird_/5437975   Published in J. Comp. Neuro: http://onlinelibrary.wiley.com/doi/10.1002/cne.24428/abstract  First posted on biorxiv: https://www.biorxiv.org/content/10.1101/197590v1  Complete dissertation: https://etd.library.emory.edu/concern/etds/8k71nh08w  ","categories": [],
+        "tags": [],
+        "url": "/datasets/2017-11-07-cerebellothalamic-thalamostriatal-songbird",
+        "teaser": null
+      },{
+        "title": "Bengalese finch song repository",
+        "excerpt":"A collection of song from four Bengalese finches recorded in the Sober lab at Emory University. Separate site for dataset: https://nickledave.github.io/bfsongrepo/ Link to data on FigShare: https://figshare.com/articles/Bengalese_Finch_song_repository/4805749 Used with hybrid-vocal-classifier library to benchmark machine learning algorithms for labeling birdsong syllables and later with the vak library to benchmark the tweetynet...","categories": [],
+        "tags": [],
+        "url": "/datasets/2017-10-18-bengalese-finch-song-repository",
+        "teaser": null
+      },{
+        "title": "Make your own `academicpages` from scratch with `jekyll`",
+        "excerpt":"In this post I document the process I went through to set up a peronsal web site with a structure similar to the academicpages theme used by many academics. I’m writing the post for my future self, and for you if you want to do the same. Why would you...","categories": [],
+        "tags": [],
+        "url": "/how-to-use-jekyll-to-set-up-your-own-academic-website/",
+        "teaser": null
+      },{
+        "title": "Thalamostriatal and cerebellothalamic pathways in a songbird, the Bengalese finch",
+        "excerpt":"Abstract The thalamostriatal system is a major network in the mammalian brain, originating principally from the intralaminar nuclei of thalamus. Its functions remain unclear, but a subset of these projections provides a pathway through which the cerebellum communicates with the basal ganglia. Both the cerebellum and basal ganglia play crucial...","categories": [],
+        "tags": [],
+        "url": "/publication/2018-03-09-thalamostriatal",
+        "teaser": null
+      },{
+        "title": "Automated annotation of birdsong with a neural network that segments spectrograms",
+        "excerpt":"Abstract Songbirds provide a powerful model system for studying sensory-motor learning. However, many analyses of birdsong require time-consuming, manual annotation of its elements, called syllables. Automated methods for annotation have been proposed, but these methods assume that audio can be cleanly segmented into syllables, or they require carefully tuning multiple...","categories": [],
+        "tags": [],
+        "url": "/publication/2022-01-20-automated-annotation",
+        "teaser": null
+      },{
+        "title": "Could simplified stimuli change how the brain performs visual search tasks? A deep neural network study",
+        "excerpt":"Abstract Visual search is a complex behavior influenced by many factors. To control for these factors, many studies use highly simplified stimuli. However, the statistics of these stimuli are very different from the statistics of the natural images that the human visual system is optimized by evolution and experience to...","categories": [],
+        "tags": [],
+        "url": "/publication/2022-06-could-simplified-stimuli",
+        "teaser": null
+      },{
+        "title": "hybrid-vocal-classifier: a Python Package to Automate Labeling of Birdsong for Behavioral Experiments",
+        "excerpt":"Talk on YouTube: https://youtu.be/BwNeVNou9-s Abstract Neuroscientists study songbirds to understand how the brain learns and produces motor skills. Like babies learning to talk, songbirds acquire their song socially during a critical period in development. To understand the neural basis of birdsong, neuroscientists carry out behavioral experiments. Often analysis requires labeling...","categories": [],
+        "tags": [],
+        "url": "/talks/2017-07-14-hybrid-vocal-classifier",
+        "teaser": null
+      },{
+        "title": "Neural Networks for Segmentation of Vocalizations",
+        "excerpt":"Neural networks for speech-to-text avoid dividing speech into segments, such as syllables, but segmenting has important applications. We compare different neural networks for segmentation of vocalizations using the song of songbirds, which we study as neuroscientists. Initial results suggest a bidirectional LSTM-CNN architecture outperforms others in both segmentation and classification....","categories": [],
+        "tags": [],
+        "url": "/talks/2017-11-27-neural-networks-for-segmentation-of-vocalizations",
+        "teaser": null
+      },{
+        "title": "Building & Replicating Models of Visual Search Behavior w/ Tensorflow, Nengo, & Scientific Python",
+        "excerpt":"Talk on YouTube: https://youtu.be/fCpQd_knWQg Abstract Animals constantly use their eyes to search their environment. What can neural networks and other cognitive models tell us about this behavior? We present two related studies that leverage scientific Python libraries to address this question. The first uses Tensorflow to replicate and extend a...","categories": [],
+        "tags": [],
+        "url": "/talks/2018-07-13-building-&-replicating-models",
+        "teaser": null
+      },{
+        "title": "Software Carpentry workshop",
+        "excerpt":"Organizer and instructor for workshop:  https://data-science-for-scientists-atl.github.io/2018-09-22-emory/  ","categories": [],
+        "tags": [],
+        "url": "/teaching/2018-09-22-software-carpentry",
+        "teaser": null
+      },{
+        "title": "Software Carpentry workshop",
+        "excerpt":"Organizer and instructor for workshop:  https://data-science-for-scientists-atl.github.io/2019-02-09-emory/  ","categories": [],
+        "tags": [],
+        "url": "/teaching/2019-02-19-software-carpentry",
+        "teaser": null
+      },{
+        "title": "Software Carpentry workshop",
+        "excerpt":"Organizer and instructor for workshop:  https://data-science-for-scientists-atl.github.io/2019-11-23-emory/  ","categories": [],
+        "tags": [],
+        "url": "/teaching/2019-11-23-software-carpentry",
+        "teaser": null
+      },{
+        "title": "Software Carpentry workshop",
+        "excerpt":"Organizer and instructor for workshop:  https://data-science-for-scientists-atl.github.io/2021-01-19-Emory/  ","categories": [],
+        "tags": [],
+        "url": "/teaching/2022-01-19-software-carpentry",
+        "teaser": null
+      },{
+        "title": "Software Carpentry workshop",
+        "excerpt":"Organizer and instructor for workshop:  https://data-science-for-scientists-atl.github.io/2022-02-03-Emory/  ","categories": [],
+        "tags": [],
+        "url": "/teaching/2022-02-03-software-carpentry",
+        "teaser": null
+      }]
